9d0f52e0a3459cec2713db8e5213c9c183723471
YouJiacheng <1503679330@qq.com>
2021-08-18 15:13:28 +0000

    fix

--- models/block.py
+++ models/block.py
@@ -1,8 +1,9 @@
 import torch
 import torch.nn as nn
 
+
 class DistanceBlock(nn.Module):
-    def __init__(self, delta, D, sigma, edge_types):
+    def __init__(self, delta: float, D: int, sigma: float, edge_types: int):
         r"""
         delta: μ_i ∈ [0, delta], delta是cutoff的最大半径
         D: 输出维数
@@ -10,13 +11,13 @@ class DistanceBlock(nn.Module):
         edge_types: 边类型数（原子对类型数）
         """
         super().__init__()
-        self.register_buffer('mu', None) # make pylance happy
+        self.register_buffer('mu', None)  # make pylance happy
         self.mu = torch.linspace(start=0, end=delta, steps=D, dtype=torch.float32)
         self.sigma = sigma
         self.gain = nn.Embedding(num_embeddings=edge_types, embedding_dim=1)
         self.offset = nn.Embedding(num_embeddings=edge_types, embedding_dim=1)
 
-    def forward(self, edge_type, edge_distance):
+    def forward(self, edge_type: torch.Tensor, edge_distance: torch.Tensor):
         r"""
         Args:
             edge_type: 边类型向量 shape=(...)
@@ -24,12 +25,13 @@ class DistanceBlock(nn.Module):
         Returns:
             shape=(..., D)
         """
-        gain, offset = self.gain(edge_type), self.offset(edge_type) # (..., 1), (..., 1)
-        d: torch.Tensor = gain * edge_distance + offset - self.mu # (..., D)
+        gain, offset = self.gain(edge_type), self.offset(edge_type)  # (..., 1), (..., 1)
+        d: torch.Tensor = gain * edge_distance + offset - self.mu  # (..., D)
         return d.div_(self.sigma).square_().exp_()
 
+
 class EmbeddingBlock(nn.Module):
-    def __init__(self, atom_embed_dim, input_dim, B, output_dim):
+    def __init__(self, atom_embed_dim: int, input_dim: int, B: int, output_dim: int):
         super().__init__()
         self.weighting = nn.Sequential(
             nn.Linear(atom_embed_dim, B),
@@ -46,8 +48,8 @@ class EmbeddingBlock(nn.Module):
             nn.Flatten(-2, -1),
             nn.Linear(output_dim, output_dim)
         )
-    
-    def forward(self, atom_embed, input):
+
+    def forward(self, atom_embed: torch.Tensor, input: torch.Tensor):
         r"""
         Args:
             atom_embed: 用于边时是source和target的onehot embedding concat, shape=(..., atom_embed_dim)
